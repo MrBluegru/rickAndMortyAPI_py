bbd8a6d80e5f56835c471ad0ffb99f0f8102e300
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-05-29 19:14:58 -0300

    Update init route

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import requests
-from fastapi import FastAPI
-from src.routers import characters, episodes
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import Response
-from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
 import os
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from src.routers import characters, episodes
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
+
 app = FastAPI()
 CORS = os.getenv("CORS")
+
 
 # Routers
 app.include_router(characters.router)
@@ -23,8 +24,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def root():
-    image_url = "https://www.vodafone.es/c/statics/imagen/img_OG_Rick_y_Morty_T4_V2.jpg"
-    response = requests.get(image_url)
-    return Response(content=response.content, media_type="image/jpeg")
+    return FileResponse("static/init.jpeg")
